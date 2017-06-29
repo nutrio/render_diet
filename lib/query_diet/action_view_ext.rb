819d17f2025e7e5e7f30f8613062abad419aa62b
Thomas "Teflon Ted" Davis <github@rudiment.net>
2017-06-29 13:37:30 -0400

    why isn't the widget seeing the nested render?

--- lib/query_diet/action_view_ext.rb
+++ lib/query_diet/action_view_ext.rb
@@ -1,13 +1,17 @@
 module QueryDiet
   module ActionViewExt
     def render(context, options)
-=begin
-       {:action=>"two_queries", :prefixes=>["query_diet", "application"], :template=>"two_queries", :layout=>#<Proc:0x007ff40c265928@/Users/ted/.rvm/gems/ruby-2.3.0/gems/actionview-5.0.0.beta2/lib/action_view/layouts.rb:386>}
-
-       {:partial=>"nested", :template=>""}
-=end
       QueryDiet::Logger.log(options[:partial] || options[:template]) { super }
     end
+=begin
+    def render_template(context, options)
+      QueryDiet::Logger.log(options[:partial] || options[:template]) { super }
+    end
+
+    def render_partial(context, options, &block)
+      QueryDiet::Logger.log(options[:partial] || options[:template]) { super }
+    end
+=end
   end
 end
 
--- spec/shared/spec/logger_spec.rb
+++ spec/shared/spec/logger_spec.rb
@@ -85,8 +85,8 @@ describe QueryDiet::Logger do
       #Movie.count.should == 0
       #Movie.create
       #Movie.count.should == 1
-      render = QueryDietController.render partial: 'nested'
-      render.should include("nested")
+      rendered = QueryDietController.render :two_queries
+      rendered.should include("nested")
     end
 
     it "should be false by default" do
